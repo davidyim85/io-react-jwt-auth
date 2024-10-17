2db54458d3857c763c0a9857baae61795a9f8483
yimd85 <yimd85@gmail.com>
2024-10-17 19:37:38 -0400

    use effect

--- src/services/hootService.js
+++ src/services/hootService.js
@@ -1,7 +1,7 @@
 const BASE_URL = `${import.meta.env.VITE_EXPRESS_BACKEND_URL}/hoots`;
 
 
-const index = async () => {
+export const index = async () => {
     try {
         const res = await fetch(BASE_URL, {
             headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
@@ -12,4 +12,7 @@ const index = async () => {
     }
 }
 
-export { index }
+ 
+export const loggingMyname = () => {
+    console.log('david')
+}
